9e325d5ea5728eb6e7529fad660e21c50aac746b
Christoffer Silva <christoffersilvadev123@gmail.com>
2024-11-17 11:16:07 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+window.alert('Bem vindo(a) !!')
+document
+.querySelector("button.fat ")
+.addEventListener("clik", function(){
+   document
+       .querySelector("#footer")
+       .classList
+       .toggle("hide")
+})
